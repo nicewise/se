877675950380deffb662a185122b789373f6dcdb
Sun Haohang(工位版) <nicewise@qq.com>
2025-05-22 21:20:24 +0800

    aa

--- elastic_damage_constitutive_test/pcwdbg.jl
+++ elastic_damage_constitutive_test/pcwdbg.jl
@@ -3,6 +3,7 @@ using Debugger
 using LinearAlgebra
 using StaticArrays
 using Plots
+using JuMP, PATHSolver
 
 struct MicroCrack
     direction::SVector{3}
@@ -46,11 +47,31 @@ compute_walpole(c::MicroCrack, x) =
     end
 end
 
+struct LCP
+    n::Int
+    model
+    function LCP(n) # n denotes the number of variables
+        model = Model(PATHSolver.Optimizer)
+        #set_optimizer_attribute(model, "output", "no")
+        set_optimizer_attribute(model, "silent", true)
+        # without this option set to "true"
+        # PATHSolver will print a lot of information to stdout
+        return new(n, model)
+    end
+end
+function LCP_solve(lcp::LCP, M, q)
+    x = @variable(lcp.model, [1:lcp.n], lower_bound = 0)
+    @constraint(lcp.model, M * x .+ q ⟂ x)
+    optimize!(lcp.model)
+    return(value.(x)) # obtain solution
+end
+
 struct AEDPCW{T<:Dim3} <: AbstractConstitutiveLaw{T}
     l::Float64
     x::Vector{Int}
     y::Vector{Int}
     n::Int # 代表性裂隙族数量，ie. 球面积分高斯点数目
+    ω::Vector{Float64}
     cracks::Vector{MicroCrack}
     C0::SMatrix{6, 6, Float64}
     Tr_gen::Function
@@ -58,6 +79,7 @@ struct AEDPCW{T<:Dim3} <: AbstractConstitutiveLaw{T}
     isopen::Function
     Rd::AbstractRd
     tol::Float64
+    lcp::LCP
     function AEDPCW{T}(E::Float64, ν::Float64, Rd::AbstractRd; tol::Float64 = 1e-6, l::Float64 = 0., n::Int) where T
         x = collect(1:n)
         y = collect(n+1:2n)
@@ -76,7 +98,8 @@ struct AEDPCW{T<:Dim3} <: AbstractConstitutiveLaw{T}
         αμ = (2k3 + 6μ2) / 5(k3 + 2μ2) / μ2
         Pd = αk * Mandel.J + αμ * Mandel.K
         isopen = σ -> [σ' * crack.N for crack in cracks] .>= 0 # σ -> vector{Bool}
-        return new{T}(l, x, y, n, cracks, C0, Tr_gen, Pd, isopen, Rd, tol)
+        lcp = LCP(n)
+        return new{T}(l, x, y, n, dnw[:, 4], cracks, C0, Tr_gen, Pd, isopen, Rd, tol, lcp)
     end
 end
 
@@ -111,6 +134,9 @@ function NCP_desent(f, nv::Int)
     return(z)
 end
 
+
+
+
 function FEM.constitutive_law_apply!(F::AEDPCW{T}, p::AbstractPoint{<:AbstractCellType, T}; before_gradient::Union{Nothing, Bool}=nothing) where T<:Dim3
     if isnothing(before_gradient) || before_gradient
         d_old = p.statev[F.x]
@@ -120,12 +146,29 @@ function FEM.constitutive_law_apply!(F::AEDPCW{T}, p::AbstractPoint{<:AbstractCe
         Tr = [F.Tr_gen(crack, stats[i]) for (i, crack) in enumerate(F.cracks)]
         Cd(x) = sum(d(x) .* Tr) # vector -> matrix
         B(x) = (Mandel.I + F.Pd * Cd(x)) \ F.Pd # vector -> matrix
-        CdB(x) = 2 * B(x) * Cd(x) - Mandel.I # vector -> matrix
-        Fd(x) = [-ε'*Tr[i]*CdB(x)*ε/2 for i in 1:F.n]
-        #g(x) = Fd(x) - Rd(F.Rd, d(x)) # vector -> vector
-        g(x) = Rd(F.Rd, d(x)) - Fd(x)# vector -> vector
-        if sum( g(zeros(F.n)) .< -F.tol ) >= 1
-            Δd = NCP_desent(g, F.n)#optim(g) # TODO
+        BCd2(x) = 2 * B(x) * Cd(x) - Mandel.I # vector -> matrix
+        Fd(x) = [-ε'*Tr[i]*BCd2(x)*ε/2 for i in 1:F.n]
+        g(x) = Fd(x) - Rd(F.Rd, d(x), F.ω) # vector -> vector
+
+        CdB(x) = Mandel.I - Cd(x) * B(x)
+        BCd(x) = Cd(x) * B(x) - Mandel.I
+        function dFd(x)
+            o = zeros(F.n, F.n)
+            for i in 1:F.n
+                o[i, i] = dFd_val(x, Tr[i], Tr[i]) / 2
+                for j in i+1:F.n
+                    o[i, j] = dFd_val(x, Tr[j], Tr[i])
+                end
+            end
+            return (o + o')
+        end
+        dFd_val(x, Tj, Ti) = -ε'*CdB(x)*(Tj*B(x)*Ti)*BCd(x)*ε #TODO
+
+        g∂d = dFd(d_old) - dRd(F.Rd, d_old, F.ω)
+        g_penetrator = g(zeros(F.n))
+        if sum( g_penetrator .> F.tol ) >= 1
+            #Δd = NCP_desent(g, F.n)#optim(g) # TODO
+            Δd = LCP_solve(F.lcp, g∂d, g_penetrator)
             d_old = d(Δd)
         end
         p.statev[F.x] .= d_old
